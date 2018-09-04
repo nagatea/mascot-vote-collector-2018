990bcd924ae577fe7c2fcd17f3e70ceb98c90732
nagatech <cheesecakebot@yahoo.co.jp>
2018-09-04 23:03:05 +0900

    GET users APIの実装

--- app.rb
+++ app.rb
@@ -1,7 +1,47 @@
 require 'sinatra/base'
+require "sinatra/json"
+require 'mysql2'
+require 'dotenv'
+
+Dotenv.load
 
 class App < Sinatra::Base
+  helpers do
+    def config
+      @config ||= {
+        db: {
+          host: ENV['DB_HOST'] || 'localhost',
+          port: ENV['DB_PORT'] || 3306,
+          username: ENV['DB_USER'] || 'root',
+          password: ENV['DB_PASSWORD'],
+          database: ENV['DB_NAME'] || 'mascot',
+        }
+      }
+    end
+
+    def db
+      return Thread.current[:mascot_db] if Thread.current[:mascot_db]
+      client = Mysql2::Client.new(
+        host: config[:db][:host],
+        port: config[:db][:port],
+        username: config[:db][:username],
+        password: config[:db][:password],
+        database: config[:db][:database],
+        reconnect: true,
+      )
+      Thread.current[:mascot_db] = client
+      client
+    end
+  end
+
   get '/' do
     'Hello world!'
+  end
+
+  get '/api/users' do
+    data = Array.new
+    db.prepare('SELECT * FROM users').execute.each { |row| data << row }
+    puts data.inspect
+    json data
   end
 end
