1c39b9df8636e5c6f0d5ac22dc1842056c7612f7
nagatech <cheesecakebot@yahoo.co.jp>
2018-09-04 23:55:45 +0900

    票数を0で初期化する

--- initialize.rb
+++ initialize.rb
@@ -37,3 +37,13 @@ base_url = 'https://contest.gakumado.mynavi.jp/mascot2018/photos/detail/'
   db.prepare('INSERT INTO users (id, name, owner_name) VALUES (?, ?, ?)').execute(id, name, owner_name)
   sleep(1)
 end
+
+date = (Time.now - 60*60*24).strftime("%Y-%m-%d")
+db.prepare('INSERT INTO dates (date) VALUES (?)').execute(date)
+date_id = db.last_id
+vote = 0
+
+(1..60).each do |id|
+  puts "date_id = #{date_id}, user_id = #{id}, vote = #{vote}"
+  db.prepare('INSERT INTO votes (date_id, user_id, vote) VALUES (?, ?, ?)').execute(date_id, id, vote)
+end
