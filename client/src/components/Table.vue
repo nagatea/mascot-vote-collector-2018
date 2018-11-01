91fada282691cce4991377c51ca449bc3a403609
nagatech <cheesecakebot@yahoo.co.jp>
2018-11-01 18:24:21 +0900

    投票期間終わったのでdateを固定

--- client/src/components/Table.vue
+++ client/src/components/Table.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2><md-icon class="md-size-2x">list_alt</md-icon>最新の集計結果({{ date.toFormat('YYYY-MM-DD') }})</h2>
+    <h2><md-icon class="md-size-2x">list_alt</md-icon>最新の集計結果(2018-10-31)</h2>
     <md-table v-model="voteData" md-sort="vote" md-sort-order="desc" md-card>
       <md-table-row slot="md-table-row" slot-scope="{ item }">
         <md-table-cell md-label="Rank" md-sort-by="rank">{{ item.rank }} <span :style="{ color: getColor(item.rank_difference) }">{{ getRankDifference(item.rank_difference) }}</span></md-table-cell>
@@ -14,20 +14,17 @@
 
 <script>
 import axios from 'axios'
-require('date-utils')
 export default {
   name: 'Table',
   data () {
     return {
-      date: null,
       voteData: [],
       oldVoteData: [],
       base_url: 'https://contest.gakumado.mynavi.jp/mascot2018/photos/detail/'
     }
   },
   async created () {
-    this.date = Date.yesterday()
-    const resp = await axios.get(`/api/votes/${this.date.toFormat('YYYY-MM-DD')}`)
+    const resp = await axios.get(`/api/votes/2018-10-31`)
     this.voteData = resp.data
   },
   methods: {
